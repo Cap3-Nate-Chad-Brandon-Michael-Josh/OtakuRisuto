d62fd3f55992d6ac1f3ac76759a3f2956da104d8
NathanielGaffney <nate440@gmail.com>
2020-11-19 11:05:26 -0500

    header iteration 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
 import { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
+import OtakuContext from '../../contexts/OtakuContext';
 
 class Header extends Component {
-    render(){
-        return(
+    static contextType = OtakuContext;
+
+    handleLogoutClick = () => {
+        this.context.processLogout();
+    };
+
+
+    render() {
+        return (
             <section className='Header'>
-                <Link to={'/home'}><h1>OtakuRisuto</h1></Link>
+                <div className='headerTitle'>
+                    <Link to={'/home'}><h1>OtakuRisuto</h1></Link>
+                    <div className='headerItems'>
+                        <div className='headerLogout'>
+                            <h2>{this.context.user.username}</h2>
+                            <Link
+                            onClick={this.handleLogoutClick}
+                            to={'/'}>
+                            Logout
+                            </Link>
+                            </div>
+                        <SearchBar />
+                    </div>
+                </div>
             </section>
         )
-    }
-}
+    };
+};
 
 export default Header;
