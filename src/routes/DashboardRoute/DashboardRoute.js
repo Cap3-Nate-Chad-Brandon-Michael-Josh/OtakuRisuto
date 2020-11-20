370aa4533e83ee43a4c780a35b2e4ddd128b1b63
Soundna <soundna@Soundnas-MacBook-Pro.local>
2020-11-20 16:48:26 -0500

    nav bar complete

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,5 +1,6 @@
 // user dashboard container
 import Header from '../../components/Header/Header'
+import DashNav from '../../components/DashNav/DashNav'
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
@@ -8,6 +9,7 @@ class DashboardRoute extends Component {
         return(
             <section className='DashboardRoute'>
                 <Header />
+                <DashNav />
                 <Link to={'/'}>
                     landing page
                 </Link>
