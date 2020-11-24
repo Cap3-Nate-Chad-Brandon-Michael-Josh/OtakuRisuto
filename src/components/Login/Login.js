e0c01493f50c84aa7fadeda4c9877ced9c0015f2
NathanielGaffney <nate440@gmail.com>
2020-11-24 12:11:24 -0500

    fixing

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,95 +1,58 @@
 //login form
 
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import AuthApiService from '../../services/auth-api-service'
+import { Link } from 'react-router-dom';
+import AuthApiService from '../../services/auth-api-service';
+import OtakuContext from '../../contexts/OtakuContext'
 import './login.css'
 class Login extends Component {
 
     static defaultProps = {
         onLoginSuccess: () => { }
-      }
-    
-      static contextType = UserContext
-    
-      state = { error: null }
-    
-      firstInput = React.createRef()
-    
-      handleSubmit = ev => {
-        ev.preventDefault()
-        const { username, password } = ev.target
-    
-        this.setState({ error: null })
-    
-        AuthApiService.postLogin({
-          username: username.value,
-          password: password.value,
-        })
-          .then(res => {
-            username.value = ''
-            password.value = ''
-            this.context.processLogin(res.authToken)
-            this.props.onLoginSuccess()
-          })
-          .catch(res => {
-            this.setState({ error: res.error })
-          })
-      }
-    
-      componentDidMount() {
-        this.firstInput.current.focus()
-      }
-    
-      render() {
-        const { error } = this.state
-        return (
-          <form
-            className='main-form center'
-            onSubmit={this.handleSubmit}
-          >
-            <div role='alert'>
-              {error && <p>{error}</p>}
-            </div>
-            <div className='LoginItem'>
-              <Label htmlFor='login-username-input'>
-                Username
-              </Label>
-              <Input
-                ref={this.firstInput}
-                id='login-username-input'
-                name='username'
-                required
-              />
-            </div>
-            <div className='LoginItem'>
-              <Label htmlFor='login-password-input'>
-                Password
-              </Label>
-              <Input
-                id='login-password-input'
-                name='password'
-                type='password'
-                required
-              />
-            </div>
-            <footer className='center'>
-            <Button className='LoginItem center' type='submit'>
-              Login
-            </Button>
-            </footer>
-          </form>
-        )
-      }
     }
 
-    render(){
-        return(
+    static contextType = OtakuContext
+
+    state = { error: null }
+
+    firstInput = React.createRef()
+
+    handleSubmit = ev => {
+        ev.preventDefault()
+        const { username, password } = ev.target
+        console.log(username.value)
+
+        this.setState({ error: null })
+
+        AuthApiService.postLogin({
+            username: username.value,
+            password: password.value,
+        })
+            .then(res => {
+                username.value = ''
+                password.value = ''
+                this.context.processLogin(res.authToken)
+                this.props.onLoginSuccess()
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
+    }
+
+    componentDidMount() {
+        this.firstInput.current.focus()
+    }
+
+    render() {
+        return (
             <form className='Login' onSubmit={this.handleSubmit}>
-                <label for="email">Email</label><br></br>
-                <input type='text' className='' placeholder='email'></input><br></br>
-                <label for="email">Password</label><br></br>
-                <input type='text' className='' placeholder='password'></input><br></br>
+                <div role='alert'>
+                    {this.state.error && <p>{this.state.error}</p>}
+                </div>
+                <label htmlFor="username" ref={this.firstInput}>username</label><br></br>
+                <input type='text' name='username' className='' placeholder='username'></input><br></br>
+                <label htmlFor="password">Password</label><br></br>
+                <input type='text' name='password' className='' placeholder='password'></input><br></br>
                 <button type='submit' className='Submit'>Login</button>
             </form>
         )
