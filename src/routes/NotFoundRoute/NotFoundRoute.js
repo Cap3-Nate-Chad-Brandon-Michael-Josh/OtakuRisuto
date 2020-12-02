3f2fd7cf137f22cb14ca4bd316b84ff87d453e3a
Chad Drake <chaddrake1313@yahoo.com>
2020-12-02 09:47:24 -0600

    public and private routes, as well as 404 page

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,1 +1,15 @@
-// 404 page
+import React, { Component } from "react";
+
+export default class NotFoundRoute extends Component {
+  render() {
+    return (
+      <section className="NotFound">
+        <h2>404</h2>
+        <p>
+          Sorry, the page you are looking for appears to be watching anime at
+          the moment. Maybe go see if another page is available!
+        </p>
+      </section>
+    );
+  }
+}
