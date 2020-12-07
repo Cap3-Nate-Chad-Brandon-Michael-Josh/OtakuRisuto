504e6f69d65106cb2580019294278f547ba3ed52
Chad Drake <chaddrake1313@yahoo.com>
2020-12-07 15:21:52 -0600

    css updates

--- src/components/SearchPublicListsResults/SearchPublicListsResults.js
+++ src/components/SearchPublicListsResults/SearchPublicListsResults.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const renderRating = (props) => {
-    let acc = Math.round(props.list.rating);
-    let res = []
-    for(let i = 0; i < 5; i++){
-        if(acc === 0){
-            res.push(<i key={i} className="far fa-star"></i>)
-        } else {
-            res.push(<i key={i} className="fas fa-star"></i>)
-            acc--;
-        }
+  let acc = Math.round(props.list.rating);
+  let res = [];
+  for (let i = 0; i < 5; i++) {
+    if (acc === 0) {
+      res.push(<i key={i} className="far fa-star"></i>);
+    } else {
+      res.push(<i key={i} className="fas fa-star"></i>);
+      acc--;
     }
-    return res;
-}
+  }
+  return res;
+};
 
 const SearchPublicListsResults = (props) => {
-    return (
-        <div>
-            <h2 onClick={event => props.viewList(event, props.list.list_id)}>{props.list.name}</h2>
-            <p>OR Average Rating: </p>
-            {renderRating(props)}
-            <p>Owned by: {props.list.owner.username}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2
+        className="result-item"
+        onClick={(event) => props.viewList(event, props.list.list_id)}
+      >
+        {props.list.name}
+      </h2>
+      <p>OR Average Rating: </p>
+      {renderRating(props)}
+      <p>Owned by: {props.list.owner.username}</p>
+    </div>
+  );
+};
 
 export default SearchPublicListsResults;
