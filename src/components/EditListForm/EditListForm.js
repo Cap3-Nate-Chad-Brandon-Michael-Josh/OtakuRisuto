764b4ba96424fa229d833756afb4c802d11cfa20
Michael Hicks Richardson <michaelhicksrichardson@Michaels-Mac-mini.local>
2020-12-08 11:10:16 -0500

    no changes

--- src/components/EditListForm/EditListForm.js
+++ src/components/EditListForm/EditListForm.js
@@ -51,7 +51,7 @@ class EditListForm extends Component {
                         value={this.state.nameInput}
                         onChange={this.handleChange}
                         required />
-                    <label htmlFor='editListSubmit'></label>
+                    <label htmlFor='editListSubmit'></label><br></br>
                     <button type='submit' name='editListSubmit' className='submitT'>Submit</button>
                 </form>
                 <button onClick={this.handleDelete} className='delete-button'>
